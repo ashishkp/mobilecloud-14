a819233c1917bc9d0efb6f438db726c04a95218b
Ashish Patel <ashish.k.patel@gmail.com>
2014-08-22 00:18:02 +0530

    Assignment 2 completed - added files

---
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
@@ -0,0 +1,23 @@
+package org.magnum.mobilecloud.video.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * An interface for a repository that can store Video
+ * objects and allow them to be searched by title and DurationLessThan
+ * 
+ * @author jules
+ *
+ */
+@Repository
+public interface VideoRepository extends CrudRepository<Video, Long>{
+
+	// Find all videos with a matching title (e.g., Video.name)
+	public Collection<Video> findByName(String title);
+	
+	// Find all videos that are shorter than a specified duration
+	public Collection<Video> findByDurationLessThan(long maxduration);
+}
